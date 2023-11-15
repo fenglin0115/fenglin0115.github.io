53bdd61dbba0919b860cd76d3e86e9b8653adccf
Feng-Lin Lu <fenglin.lu@gmail.com>
2023-11-15 22:26:37 +0800

    update

---
+++ proportion/GeoGebra/HTML5/5.0/web/deferredjs/821D11ACA9E966DCF9529086B74E3506/3.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback3('FAc(3486,1,zDm);_.te=function(){};iPl(ki)(3);\n//# sourceURL=web-3.js\n')
---
+++ proportion/GeoGebra/HTML5/5.0/webSimple/deferredjs/88B3F37606A6FCC29F7736201A65B366/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('mAb(1955,1,qEh);_.zd=function(){};K2g(Ng)(2);\n//# sourceURL=webSimple-2.js\n')
